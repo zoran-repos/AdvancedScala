4c2b63e77a4fd1854c393f06fd7990e602aa7425
zoran-repos <trendo@hotmail.com>
2024-11-11 10:29:14 +0100

    --- adding filterDivisibleBy3

--- src/main/scala/HigherOrderFunctionExample.scala
+++ src/main/scala/HigherOrderFunctionExample.scala
@@ -6,16 +6,22 @@ object HigherOrderFunctionExample {
     (list: List[Int]) => list.filter(predicate)
   }
 
-  // Predicate --- show with will happen if remove type parameter
+  // Predicate to check if a number is even
   val isEven: Int => Boolean = _ % 2 == 0
 
-  // Function that filters even numbers
-  val filterEvens = createFilter(isEven)
+  // Predicate to check if a number is divisible by 3
+  val isDivisibleBy3: Int => Boolean = _ % 3 == 0
 
+  // Function that filters even numbers
+  private val filterEvens: List[Int] => List[Int] = createFilter(isEven)
+
+  private val filterDivisibleBy3 = createFilter(isDivisibleBy3)
 
   def main(args: Array[String]): Unit = {
     val numbers = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     val evenNumbers = filterEvens(numbers)
+    val every3Numbers = filterDivisibleBy3(numbers)
     println(evenNumbers)
+    println(every3Numbers)
   }
 }
