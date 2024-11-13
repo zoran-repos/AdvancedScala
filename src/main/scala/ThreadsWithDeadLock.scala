7348958c9c6850abc24ff75d8bcc5ed1e0341189
zoran-repos <trendo@hotmail.com>
2024-11-13 11:28:33 +0100

    --- Chapter4 - changes

--- src/main/scala/ThreadsWithDeadLock.scala
+++ src/main/scala/ThreadsWithDeadLock.scala
@@ -5,40 +5,36 @@ import java.util.concurrent.locks.ReentrantLock
   val lock2 = new ReentrantLock()
 
   // Thread 1 acquires lock1 and then tries to acquire lock2
-  val thread1 = new Thread(new Runnable {
-    def run(): Unit = {
-      lock1.lock()
+  val thread1 = new Thread(() => {
+    lock1.lock()
+    try {
+      println("Thread 1 acquired lock1, waiting for lock2...")
+      Thread.sleep(100) // Simulate some work
+      lock2.lock()
       try {
-        println("Thread 1 acquired lock1, waiting for lock2...")
-        Thread.sleep(100) // Simulate some work
-        lock2.lock()
-        try {
-          println("Thread 1 acquired lock2")
-        } finally {
-          lock2.unlock()
-        }
+        println("Thread 1 acquired lock2")
       } finally {
-        lock1.unlock()
+        lock2.unlock()
       }
+    } finally {
+      lock1.unlock()
     }
   })
 
   // Thread 2 acquires lock2 and then tries to acquire lock1
-  val thread2 = new Thread(new Runnable {
-    def run(): Unit = {
-      lock2.lock()
+  val thread2 = new Thread(() => {
+    lock2.lock()
+    try {
+      println("Thread 2 acquired lock2, waiting for lock1...")
+      Thread.sleep(100) // Simulate some work
+      lock1.lock()
       try {
-        println("Thread 2 acquired lock2, waiting for lock1...")
-        Thread.sleep(100) // Simulate some work
-        lock1.lock()
-        try {
-          println("Thread 2 acquired lock1")
-        } finally {
-          lock1.unlock()
-        }
+        println("Thread 2 acquired lock1")
       } finally {
-        lock2.unlock()
+        lock1.unlock()
       }
+    } finally {
+      lock2.unlock()
     }
   })
 
