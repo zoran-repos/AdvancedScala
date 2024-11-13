699289948ab3186f9c578730c118ec56ad436d65
zoran-repos <trendo@hotmail.com>
2024-11-13 11:05:35 +0100

    --- Chapter3 - changes

--- src/main/scala/ImplicitParameterAsLogger.scala
+++ src/main/scala/ImplicitParameterAsLogger.scala
@@ -1,13 +1,18 @@
 case class LogLevel(level: String)
 
 object LoggerExample {
-  def logMessage(message: String)(implicit logLevel: LogLevel): Unit = {
+  private def logMessage(message: String)(implicit logLevel: LogLevel): Unit = {
     println(s"[${logLevel.level}] - $message")
   }
 
   def main(args: Array[String]): Unit = {
     // Defining the default log level as `INFO`
     implicit val defaultLogLevel: LogLevel = LogLevel("INFO")
+    // this will be Ambiguous implicit values:
+    // both value defaultLogLevel of type LogLevel and value defaultLogLevel2 of type LogLevel
+    // match expected type LogLevel
+
+    //implicit val defaultLogLevel2: LogLevel = LogLevel("DEBUG")
 
     // Using the function without an explicit level
     logMessage("Application started.") 
